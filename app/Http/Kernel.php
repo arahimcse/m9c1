828db78944a3b3af3cb48d698e0435007c00d02c
arahimcse <arahimcse@gamil.com>
2024-02-19 00:21:12 +0600

    Before Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'simple' => \App\Http\Middleware\SimpleMiddleware::class,
+        'beforChcek' => \App\Http\Middleware\BeforeMiddleware::class,
     ];
 }
